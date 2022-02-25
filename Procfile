6ad724be75206c3219da7c70a4de2b0d138ad1c9
strikefvela <strikefvela@hotmail.com>
2022-02-25 14:49:04 -0800

    Added docker compose with postgres db and postgres admin images for dev along with a db init script.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python backend/server.py
---
+++ db_init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE ukraine_will_stand
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
