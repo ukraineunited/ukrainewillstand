b4d2a36cd8538d71532c6c535cb37296c9c6a48a
rotem <psyberryd@gmail.com>
2022-02-28 10:07:21 +0200

    add Romanian form parsing.

--- backend/json_to_form_parser.py
+++ backend/json_to_form_parser.py
@@ -1,14 +1,11 @@
 from docxtpl import DocxTemplate
+from collections import defaultdict
 from os import path
 
 
 PLACE_OF_STAY_KEY = "placeOfStay"
 CONTACT_INFO_KEY = "contactInfo"
 PERSONAL_KEY = "personal"
-CITY_KEY = "city"
-STREET_KEY = "street"
-TEL_MOTHER_KEY = "mothersPhone"
-TEL_FATHER_KEY = "fathersPhone"
 SOURCE_FOLDER = path.join("backend", "documents")
 DESTINATION_FOLDER = path.join(SOURCE_FOLDER, "completed")
 
@@ -18,20 +15,35 @@ def fill_polish_form(json):
     Fills the polish form from the given json and saves it as a docx. May throw KeyError if any
     expected key was missing.
     :param json: Assumed to be dictionary mapping strings to dictionaries, containing the
-    keys "PlaceOfStay", "contactInfo" and "personal"
+    keys "placeOfStay", "contactInfo" and "personal"
     """
     polish_form = DocxTemplate(path.join(SOURCE_FOLDER, "PolishForm.docx"))
+    fill_and_save_template_from_data(polish_form, decompose_json(json))
 
-    place_of_stay = json[PLACE_OF_STAY_KEY]
-    contact_info = json[CONTACT_INFO_KEY]
-    context = {
-        STREET_KEY: place_of_stay[STREET_KEY],
-        CITY_KEY: place_of_stay[CITY_KEY],
-        TEL_FATHER_KEY: contact_info[TEL_FATHER_KEY],
-        TEL_MOTHER_KEY: contact_info[TEL_MOTHER_KEY]
-    }
-    polish_form.render(context)
-    personal = json[PERSONAL_KEY]
-    name = " ".join((personal["firstName"], personal["lastName"]))
-    polish_form.save(path.join(DESTINATION_FOLDER, name + ".docx"))
 
+def fill_romanian_form(json):
+    """
+    Fills the Romanian form from the given json and saves it as a docx. May throw KeyError if any
+    expected key was missing.
+    :param json: Assumed to be dictionary mapping strings to dictionaries, containing the
+    keys "placeOfStay", "contactInfo" and "personal"
+    """
+    romanian_form = DocxTemplate(path.join(SOURCE_FOLDER, "RomanianForm.docx"))
+    fill_and_save_template_from_data(romanian_form, decompose_json(json))
+
+
+def decompose_json(json):
+    """
+    Decomposes the given json to its components. May throw KeyError if any expected key was missing.
+    :param json: Assumed to be dictionary mapping strings to dictionaries, containing the
+    keys "placeOfStay", "contactInfo" and "personal".
+    :return: A defaultdict containing all the data from each component.
+    """
+    return defaultdict(str, **json[PLACE_OF_STAY_KEY], **json[CONTACT_INFO_KEY],
+                       **json[PERSONAL_KEY])
+
+
+def fill_and_save_template_from_data(template, data):
+    template.render(data)
+    name = " ".join((data["firstName"], data["lastName"]))
+    template.save(path.join(DESTINATION_FOLDER, name + ".docx"))
